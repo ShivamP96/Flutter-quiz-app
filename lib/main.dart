7b8ad725bc5edab3d68869c8249bc884b706aa22
Shivam Patel <patelsd96@gmail.com>
2023-03-26 17:17:45 -0400

    add in some changes to the basic flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// void main() {
+//   runApp(MyApp());
+// }
+
+// use this syntax for anything that is one line
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // we are  overriding the statelesswidget build method
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // home is named argument, text is a positional argument
       home: Text('Hello!'),
     );
   }
